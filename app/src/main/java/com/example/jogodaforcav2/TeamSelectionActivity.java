4867b8eda6506b67acbcb1f63ffc13524c3b939c
AtellaThiago <160648495+ThiagoAtella@users.noreply.github.com>
2025-04-19 18:30:34 -0300

    teste

---
+++ app/src/main/java/com/example/jogodaforcav2/TeamSelectionActivity.java
@@ -0,0 +1,61 @@
+package com.example.jogodaforcav2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.jogodaforcav2.databinding.ActivityTeamSelectionBinding;
+
+public class TeamSelectionActivity extends AppCompatActivity {
+    private ActivityTeamSelectionBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTeamSelectionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonFluminense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJogo("FLUMINENSE");
+            }
+        });
+
+        binding.buttonFlamengo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJogo("FLAMENGO");
+            }
+        });
+        binding.buttonVasco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJogo("VASCO");
+            }
+        });
+        binding.buttonBotafogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJogo("BOTAFOGO");
+            }
+        });
+        binding.buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voltarpromenu(); // Volta para o menu
+            }
+        });
+    }
+
+    private void iniciarJogo(String time) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("TIME_ESCOLHIDO", time);
+        startActivity(intent);
+    }
+
+    private void voltarpromenu() {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+}
